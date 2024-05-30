3a9480e1f1f0853d94db4472eca1c3d3b2456727
Ethan Arrowood <ethan@arrowood.dev>
2024-05-29 22:44:04 -0600

    fix css for timeline list

--- ethanarrowood.com/app/components/Timeline.tsx
+++ ethanarrowood.com/app/components/Timeline.tsx
@@ -13,9 +13,9 @@ function Event ({
   last: boolean
 }) {
   return (
-    <li className={`ms-4 ${last ? "" : "mb-4"}`}>
-      <div className="absolute w-3 h-3 bg-emerald-700 rounded-full mt-1/2 -start-1.5 border border-emerald-700"></div>
-      <p className="mb-1 md:text-sm text-xs font-normal leading-none text-gray-400">{event.date}</p>
+    <li className={`ms-4 ${last ? "" : "mb-8"}`}>
+      <div className="absolute md:w-3.5 md:h-3.5 w-3 h-3 bg-emerald-700 rounded-full -start-1.5 border border-emerald-700"></div>
+      <p className="mb-1 md:leading-none md:text-sm text-xs font-normal leading-none text-gray-400">{event.date}</p>
       <h3 className="md:text-base text-sm font-semibold text-gray-900">{event.title}</h3>
       {
         event.description && event.description.length > 0 ? (
@@ -43,7 +43,7 @@ export function Timeline ({
   return (
     <ol className={`ms-2 relative border-s border-emerald-700 ${subTimeline ? 'border-dashed mt-4' : ''}`}>
       {
-        events.map((event, i) => <Event key={event.title} event={event} last={i === events.length}/>)
+        events.map((event, i) => <Event key={event.title} event={event} last={i === events.length - 1}/>)
       }
     </ol>
   )
