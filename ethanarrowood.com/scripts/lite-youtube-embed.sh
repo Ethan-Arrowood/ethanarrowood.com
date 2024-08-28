841bbd166b1739dbf15b1b85e22308d63b251ca7
Ethan Arrowood <ethan@arrowood.dev>
2024-08-27 22:24:04 -0600

    clean up

--- ethanarrowood.com/scripts/lite-youtube-embed.sh
+++
@@ -1,2 +0,0 @@
-rm -r site/lite-youtube-embed
-cp -r node_modules/lite-youtube-embed/src site/lite-youtube-embed
---
+++ ethanarrowood.com/scripts/lite-youtube.sh
@@ -0,0 +1,2 @@
+rm site/lite-youtube/lite-youtube.js
+cp node_modules/@justinribeiro/lite-youtube/lite-youtube.js site/lite-youtube/
