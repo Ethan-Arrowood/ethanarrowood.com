42d19b1909feb7ad94ca3d2e5de44a6bc8fae94a
Ethan Arrowood <ethan@arrowood.dev>
2025-07-11 23:15:00 -0600

    update site for latest conference video, update some parts of main page, and add math rendering to posts

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import { remarkDatePlugin } from "./src/utils/remarkDatePlugin.js";
 import { remarkAlert } from "remark-github-blockquote-alert";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
 	markdown: {
-		remarkPlugins: [remarkDatePlugin, remarkAlert],
+		remarkPlugins: [remarkDatePlugin, remarkAlert, remarkMath],
+		rehypePlugins: [rehypeKatex]
 	},
 });
